66e75186ac373df854b148cc8c48a062bfe4b4a8
mao <1296193245@qq.com>
2023-06-13 19:43:06 +0800

    data_visualization_echarts_line_chart

---
+++ src/main/java/mao/data_visualization_echarts_line_chart/DataVisualizationEchartsLineChartApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_echarts_line_chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationEchartsLineChartApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationEchartsLineChartApplication.class, args);
+    }
+
+}
